4193079639bce5b7659768e77514133a9884c9f6
Akrosh Tiwari <metiwariakrosh02@gmail.com>
2022-12-14 22:04:31 +0545

    small change

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,9 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as fs from 'node:fs';
+import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readFile("../blogdata/learn-javascript.json", 'utf-8', (err, data) => {
-    console.log(JSON.parse(data))
-    res.status(200).json(JSON.parse(data))
-  })
+export default async function handler(req, res) {
+
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    // console.log(item)
+    myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+    allBlogs.push(JSON.parse(myfile))
+  }
+  res.status(200).json(allBlogs)
+
+  // fs.readdir("blogdata", (err, data) => {
+  //   console.log('data')
+  //   let allBlogs = []
+  //   data.forEach((item) => {
+  //     console.log(item)
+  //     fs.readFile(('blogdata/' + item), (d) => {
+  //       allBlogs.push(d)
+  //     })
+  //   })
+  //   res.status(200).json(data)
+  // })
 }
